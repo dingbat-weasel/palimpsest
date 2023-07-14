e2b711108de27d88bc20635c2fb1d68b7576a08c
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-07-13 18:42:55 -0700

    working through next tutorial, creating pages that link to one another

---
+++ pages/index.js
@@ -0,0 +1,68 @@
+import Link from 'next/link';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className='container'>
+      <Head>
+        <title>palimpsest</title>
+      </Head>
+      <main>
+        <h1 className='title'>
+          Read <Link href='/posts/first-post'>this page!</Link>
+        </h1>
+      </main>
+
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
